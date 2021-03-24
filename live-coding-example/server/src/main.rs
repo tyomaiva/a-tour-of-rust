c14e0b214dd73ae97e1f8111ac98c857aa01aa10
aivashko <aivashko@aivashko-ASUS-laptop.home>
2021-03-24 21:17:06 +0100

    Cleaned up the code

--- live-coding-example/server/src/main.rs
+++ live-coding-example/server/src/main.rs
@@ -1,64 +1,77 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{ Value };
+use clap::{load_yaml, App};
+use serde::Deserialize;
+use serde_json::Value;
 use std::collections::HashMap;
 use std::net::UdpSocket;
 use std::{thread, time};
-use clap::{App, load_yaml};
 
-#[derive(Serialize, Deserialize)]   
-struct ExternalCommand
-{
+#[derive(Deserialize)]
+struct ExternalCommand {
     command_name: String,
-    params: HashMap<String, Value>
+    params: HashMap<String, Value>,
 }
 
-fn worker_task ( command_str:String )
-{
-    let command: ExternalCommand = serde_json::from_str(command_str.as_str()).expect("Can't continue without data");
-    let amount_of_time_to_wait = command.params.get("amount_of_time_to_wait").expect("Needs this param");
+fn worker_task(command_str: String) {
+    let command: ExternalCommand =
+        serde_json::from_str(command_str.as_str()).expect("Can't continue without proper data!");
+    println!("The command is {}", command.command_name);
+    let time_to_wait = command
+        .params
+        .get("amount_of_time_to_wait")
+        .expect("The time parameter is required!")
+        .as_u64()
+        .unwrap();
 
     let current_thread_index = rayon::current_thread_index().unwrap_or(0);
-    let time_to_wait = amount_of_time_to_wait.as_i64().unwrap() as u64;
 
-    println!("Thread#: {}, param: {}", current_thread_index, &time_to_wait);
+    println!(
+        "Thread#: {}, time to wait: {}",
+        current_thread_index, &time_to_wait
+    );
     let sleep_duration = time::Duration::from_secs(time_to_wait);
     thread::sleep(sleep_duration);
+    println!("Command executed!");
 }
 
-fn serve(n_threads: i64, port: String ) 
-{
-    let pool = rayon::ThreadPoolBuilder::new().num_threads(n_threads as usize).build().unwrap();
+fn serve(n_threads: usize, port: String) {
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(n_threads)
+        .build()
+        .unwrap();
 
     let remote = format!("127.0.0.1:{}", port);
-    let socket: UdpSocket = UdpSocket::bind(remote).expect("Couldn't create socket");
+    let socket = UdpSocket::bind(remote).expect("Couldn't create a socket");
 
-    let mut buf = [0; 1024];
+    let mut buf = [0u8; 1024];
 
     loop {
         match socket.recv(&mut buf) {
             Ok(received) => {
-                let received_str: String = String::from_utf8((&buf[..received]).to_vec()).unwrap_or(String::from("{}"));
-                println!("received {} bytes {}", received, &received_str);
-                buf = [0; 1024];
-
+                let received_str =
+                    String::from_utf8((&buf[..received]).to_vec()).unwrap_or(String::from("{}"));
+                println!("received {} bytes: {}", received, &received_str);
                 pool.spawn(move || worker_task(received_str));
-            },
+            }
             Err(e) => {
                 println!("recv function failed: {:?}", e);
                 break;
-            },
+            }
         }
+        buf = [0u8; 1024];
     }
     println!("Bye");
 }
 
 fn main() {
-
     let yaml = load_yaml!("options.yaml");
     let matches = App::from(yaml).get_matches();
 
-    let port_str = matches.value_of("port").expect("Port can't be none it is required");
-    let n_worker = matches.value_of("n_worker").unwrap_or("8").parse::<i64>().expect("No funny biz with worker number");
+    let port_str = matches.value_of("port").unwrap();
+    let n_worker = matches
+        .value_of("n_worker")
+        .unwrap_or("4")
+        .parse::<usize>()
+        .expect("Please provide a valid number for the number of threads!");
 
     serve(n_worker, String::from(port_str));
 }
